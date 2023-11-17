5b580f7384a807fdac813194fa8af7277d6c80a5
Dmitry Zheltkov <dmitry.zheltkov@gmail.com>
2023-11-17 10:57:49 +0300

    Updated, works for full rank Jacobian

--- include/ALS.hpp
+++ include/ALS.hpp
@@ -3,75 +3,82 @@
 
 #include <cstdint>
 #include <limits>
+#include <algorithm>
+#include <CXXBLAS.hpp>
+#include <CXXLAPACK.hpp>
+#include <iostream>
 
 template<class DataType>
 struct ALSParams
 {
-    typedef RealType decltype(std::abs(DataType(0.0)));
-    uint64_t block_size = 1024;
+    typedef decltype(std::abs(DataType(0.0))) RealType;
+    uint64_t block_size = 16384;
     uint64_t max_it = std::numeric_limits<uint64_t>::max();
     RealType rel_tol = 1000.0 * std::numeric_limits<RealType>::epsilon();
     RealType abs_tol = 0.0;
-}
+};
 
 template <class Model, class DataType>
-void ALS(Model &model, const DataType *rhs, const ALSParams<DataType> &params)
+void ALS(Model &model, const DataType *rhs, const ALSParams<DataType> &params = ALSParams<DataType>())
 {
-    typedef RealType decltype(std::abs(DataType(0.0)));
+    typedef decltype(std::abs(DataType(0.0))) RealType;
     const uint64_t D = model.dimensionality();
     const auto M = model.sizes();
     const uint64_t L = model.linear_size();
 
     const uint64_t K = model.length();
 
-    const uint64_t max_M = std::max_element(M, M + D);
+    const uint64_t max_M = *std::max_element(M.begin(), M.begin() + D);
 
-    const uint64_t max_JN = max_L + L;
+    const uint64_t max_JN = max_M + L;
 
     const uint64_t block_size = std::min(params.block_size, K);
 
-    const rel_tol = params.rel_tol;
-    const abs_tol = params.abs_tol;
+    const double rel_tol = params.rel_tol;
+    const double abs_tol = params.abs_tol;
     uint64_t max_it = params.max_it;
 
     std::vector<DataType> J(block_size * max_JN);
     std::vector<DataType> B(max_JN);
     std::vector<DataType> H(max_JN * max_JN);
 
-    nrm_squared = BLAS::nrm2(K, rhs, 1);
+    auto nrm_squared = BLAS::nrm2(K, rhs, 1);
     nrm_squared *= nrm_squared;
 
     uint64_t d = 0;
     uint64_t i = 0;
     bool left_to_right = true;
 
-    prev_err_squared = nrm_squared;
+    auto prev_err_squared = nrm_squared;
     while (true)
     {
         const uint64_t JN = M[d] + L;
-        std::fill(B.begin(), B.begin() + JN, DataType(0.0));
-        std::fill(H.begin(), H.begin() + JN * JN), DataType(0.0);
+        std::fill(B.begin(), B.begin() + max_JN, DataType(0.0));
+        std::fill(H.begin(), H.begin() + max_JN * max_JN, DataType(0.0));
 
         for (uint64_t k = 0; k < K; k += block_size)
         {
+            std::fill(J.begin(), J.begin() + block_size * max_JN, DataType(0.0));
             uint64_t k_last = std::min(k + block_size, K);
             model.JacobianPart(d, k, k_last, J.begin(), block_size);
             model.LinearPart(k, k_last, J.begin() + block_size * M[d], block_size);
 
             const uint64_t JM = k_last - k;
 
-            BLAS::gemv('C', JM, JN, DataType(1.0), J.data(), block_size, rhs, 1, DataType(1.0), B.data(), 1);
-            BLAS::herk('R', 'C', JN, JM, RealType(1.0), J.data(), block_size, RealType(1.0), H.data(), JN);
-        }
+            BLAS::gemv('C', JM, JN, DataType(1.0), J.data(), block_size, rhs + k, 1, DataType(1.0), B.data(), 1);
+            BLAS::herk('U', 'C', JN, JM, RealType(1.0), J.data(), block_size, RealType(1.0), H.data(), JN);
+       }
 
-        if (LAPACK::potrf('R', JN, H.data(), JN) != 0)
+
+        int info = LAPACK::potrf('U', JN, H.data(), JN);
+        if (info != 0)
         {
             throw std::runtime_error("LAPACK::potrf failed");
         }
-        BLAS::trsv('R', 'C', 'N', JN, H.data(), JN, B.data(), 1);
-        auto err_squared = BLAS::nrm2(JN, U.data(), 1);
-        err_squared = 1 - err_squared * err_squared;
-        BLAS::trsv('R', 'N', 'N', JN, H.data(), JN, B.data(), 1);
+        BLAS::trsv('U', 'C', 'N', JN, H.data(), JN, B.data(), 1);
+        auto err_squared = BLAS::nrm2(JN, B.data(), 1);
+        err_squared = nrm_squared - err_squared * err_squared;
+        BLAS::trsv('U', 'N', 'N', JN, H.data(), JN, B.data(), 1);
         model.update(d, B.begin());
         model.update_linear(B.begin() + M[d]);
         if (left_to_right)
@@ -89,7 +96,7 @@ void ALS(Model &model, const DataType *rhs, const ALSParams<DataType> &params)
             {
                 left_to_right = true;
                 i++;
-                std::cout << i << ' ' << std::log10(err_squared) - std::log10(nrm_squared) << std::endl;
+                std::cout << i << ' ' << 10 * (std::log10(err_squared) - std::log10(nrm_squared)) << std::endl;
 
                 if ((prev_err_squared - err_squared) <= std::min(abs_tol, rel_tol * nrm_squared) || i == max_it)
                 {
